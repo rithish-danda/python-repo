241f2a3481d929a936e384e91b7fa35fd21a8fc5
heyrithish <96982556+heyrithish@users.noreply.github.com>
2022-03-05 20:55:20 +0530

    temp.py
    A random first line of program in python

---
+++ temp.py
@@ -0,0 +1,13 @@
+name = input("What is Your name? ")
+age = input("what is your Age? ")
+print(name)
+print(age)
+
+super_hero = input("What is your superhero name? ")
+print("Superhero name is "+super_hero)
+
+
+"""
+  Trying out a multi line comment 
+  using string as python ignores strings if not used in syntax
+"""
