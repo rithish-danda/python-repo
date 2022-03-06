8730b16a4ca383addbb7f134bf58d7754d2a6f77
heyrithish <96982556+heyrithish@users.noreply.github.com>
2022-03-06 12:03:52 +0530

    To find greater value among two numbers

---
+++ greater_in_2.py
@@ -0,0 +1,11 @@
+# program to find greatest between teo numbers
+
+# taking inputs
+first = input("Enter first number: ")
+second = input("Enter second number: ")
+
+# selection and printing
+if first > second:
+    print("Greater number among "+first+" and "+ second+ " is "+ first)
+else:
+    print("Greater number among "+first+" and "+ second+ " is "+ second)
